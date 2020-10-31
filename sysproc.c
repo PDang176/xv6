6be6243caadea8d8b59bc1b38fd84336afe4e54e
Patrick Dang <pdang011@ucr.edu>
2020-10-30 22:36:43 -0700

    Finished lab1

--- sysproc.c
+++ sysproc.c
@@ -27,6 +27,46 @@ sys_wait(void)
 }
 
 int
+sys_exitS(void)
+{
+	int status;
+
+	if(argint(0, &status) < 0)
+		return -1;
+	exitS(status);
+	return 0;
+}
+
+int
+sys_waitS(void)
+{
+	int *status;
+
+	if(argptr(0, (char**)&status, sizeof(int*) < 0))
+		return -1;
+	return waitS(status);
+}
+
+int
+sys_waitpid(void)
+{
+	int pid;
+	int *status;
+	int options;
+
+	if(argint(0, &pid) < 0)
+    return -1;
+
+	if(argptr(1, (char**)&status, sizeof(int*) < 0))
+    return -1;	
+
+	if(argint(2, &options) < 0)
+    return -1;	
+
+	return waitpid(pid, status, options);
+}
+
+int
 sys_kill(void)
 {
   int pid;
--- user.h
+++ user.h
@@ -23,6 +23,9 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int	exitS(int) __attribute__((noreturn));
+int waitS(int*);
+int waitpid(int, int*, int);
 
 // ulib.c
 int stat(const char*, struct stat*);
